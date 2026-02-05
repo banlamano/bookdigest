512616d535a2c0626cff4827cb7ec799b142dd45
eric <erichjef@gmail.com>
2026-02-05 19:43:14 +0100

    Fix: Remove deprecated cacheTime from useQuery

--- frontend/src/components/home/FeaturedBooks.tsx
+++ frontend/src/components/home/FeaturedBooks.tsx
@@ -14,7 +14,6 @@ export function FeaturedBooks() {
     queryKey: ['featured-books'],
     queryFn: () => booksAPI.getFeatured(),
     staleTime: 5 * 60 * 1000, // 5 minutes
-    cacheTime: 10 * 60 * 1000, // 10 minutes
   });
 
   const books = data?.data?.data?.books || [];
